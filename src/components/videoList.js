693945e5b8e12133106efeeca4f6824c5c7a4080
paarth17032000 <paarth17032000@gmail.com>
2020-09-29 23:53:03 +0530

    changes

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,11 +2,31 @@ import React from 'react'
 
 export default function VideoList(videos) {
     return (
-            {videos.map( video => {
-                    <div>
-                        <img alt="thumbnail" src={video.snippet.thumbnails.middle.url}/>
-                        <h5>{video.snippet.title}</h5>
-                    </div>
-            })}
+        {videos.map((video) => (
+            <div>
+                <img alt="thumbnail" src={video.snippet.thumbnails.middle.url}/>
+                <h5>{video.snippet.title}</h5>
+            </div>
+        ))}
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// videos.map( video => {
+//     <div key={video.id.videoId}>
+//         <img alt="thumbnail" src={video.snippet.thumbnails.middle.url}/>
+//         <h5>{video.snippet.title}</h5>
+//     </div>
+// })
